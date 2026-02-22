94149a4aa9d9775a774318eebf490b2503b53066
Iklon <75901005+SquidZero@users.noreply.github.com>
2026-02-22 18:05:19 -0500

    Stuff that helped camera jittering. Player controller tweaks to make it work better with slopes. Pressing the U key now unlocks the cursor.

--- Scripts/CameraLook.cs
+++ Scripts/CameraLook.cs
@@ -1,38 +1,40 @@
-using System;
 using Godot;
 
 public partial class CameraLook : Camera3D
 {
-	Vector2 mouseDelta = Vector2.Zero;
+    Vector2 mouseDelta = Vector2.Zero;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (
-			(RotationDegrees.X + mouseDelta.Y >= 90 && mouseDelta.Sign().Y >= 0)
-			|| (RotationDegrees.X + mouseDelta.Y <= -90 && mouseDelta.Sign().Y <= 0)
-		)
-			mouseDelta[1] = 0;
-		RotateX(mouseDelta.Y);
-		((Node3D)GetParent()).RotateY(mouseDelta.X);
-		mouseDelta = Vector2.Zero;
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta)
+    {
+        if (
+            (RotationDegrees.X + mouseDelta.Y >= 90 && mouseDelta.Sign().Y >= 0)
+            || (RotationDegrees.X + mouseDelta.Y <= -90 && mouseDelta.Sign().Y <= 0)
+        )
+            mouseDelta[1] = 0;
+        RotateX(mouseDelta.Y);
+        ((Node3D)GetParent()).RotateY(mouseDelta.X);
+        mouseDelta = Vector2.Zero;
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseMotion motion)
-		{
-			mouseDelta =
-				(RotationDegrees.X + mouseDelta.Y >= 90 && mouseDelta.Sign().Y >= 0)
-				|| (RotationDegrees.X + mouseDelta.Y <= -90 && mouseDelta.Sign().Y <= 0)
-					? -motion.Relative / 300
-					: -motion.Relative / 300;
-		}
-	}
+        if (Input.IsActionJustPressed("unfocus_mouse"))
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion motion)
+        {
+            mouseDelta =
+                (RotationDegrees.X + mouseDelta.Y >= 90 && mouseDelta.Sign().Y >= 0)
+                || (RotationDegrees.X + mouseDelta.Y <= -90 && mouseDelta.Sign().Y <= 0)
+                    ? -motion.Relative / 300
+                    : -motion.Relative / 300;
+        }
+    }
 }
--- Scripts/PlayerMove.cs
+++ Scripts/PlayerMove.cs
@@ -1,54 +1,52 @@
-using System;
-using System.Linq;
 using Godot;
 
 // testicles
 public partial class PlayerMove : CharacterBody3D
 {
-	[Export]
-	public Camera3D playerCamera;
+    [Export]
+    public Camera3D playerCamera;
 
-	[Export]
-	public RayCast3D rayCast;
-	Vector2 movementAxis = Vector2.Zero;
-	float velocityY = 0f;
-	bool sprinting = false;
-	Vector3 from = Vector3.Zero;
-	Vector3 to = Vector3.Zero;
+    [Export]
+    public RayCast3D rayCast;
+    Vector2 movementAxis = Vector2.Zero;
+    float velocityY = 0f;
+    bool sprinting = false;
+    Vector3 from = Vector3.Zero;
+    Vector3 to = Vector3.Zero;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta)
-	{
-		movementAxis = Input.GetVector("left", "right", "forward", "backward");
-		sprinting = Input.IsActionPressed("sprint");
-		movementAxis *= sprinting ? (float)delta * 750 : (float)delta * 250;
-		if (IsOnFloor())
-		{
-			if (Input.GetActionStrength("jump") > 0)
-			{
-				velocityY = 6f;
-			}
-		}
-		else
-		{
-			velocityY += GetGravity().Y;
-		}
-		//GD.Print("movementVec: " + movementVec);
-		Velocity = new Vector3(movementAxis.X, velocityY, movementAxis.Y).Rotated(
-			Vector3.Up,
-			Rotation.Y
-		);
-		MoveAndSlide();
-		//GD.Print("input: " + movementAxis);
-		//GD.Print("position: " + GlobalPosition);
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta)
+    {
+        movementAxis = Input.GetVector("left", "right", "forward", "backward");
+        sprinting = Input.IsActionPressed("sprint");
+        movementAxis *= sprinting ? (float)delta * 750 : (float)delta * 250;
+        if (IsOnFloor())
+        {
+            if (Input.GetActionStrength("jump") > 0)
+            {
+                velocityY = 6f;
+            }
+        }
+        else
+        {
+            velocityY += GetGravity().Y;
+        }
+        //GD.Print("movementVec: " + movementVec);
+        Velocity = new Vector3(movementAxis.X, velocityY, movementAxis.Y).Rotated(
+            Vector3.Up,
+            Rotation.Y
+        );
+        MoveAndSlide();
+        //GD.Print("input: " + movementAxis);
+        //GD.Print("position: " + GlobalPosition);
 
-		if (Input.IsActionJustReleased("shoot"))
-		{
-			var col = rayCast.GetCollider();
-			col?.EmitSignal("take_damage", 0.1f);
-		}
-	}
+        if (Input.IsActionJustReleased("shoot"))
+        {
+            var col = rayCast.GetCollider();
+            col?.EmitSignal("take_damage", 0.1f);
+        }
+    }
 }

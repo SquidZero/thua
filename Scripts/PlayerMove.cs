24f2a264978b9fc53c8313a8c0a42faf7affbd0e
Sherman-tank4000 <matias.farrell@icloud.com>
2026-02-24 17:21:20 -0500

    i forgor

--- Scripts/PlayerMove.cs
+++ Scripts/PlayerMove.cs
@@ -17,7 +17,7 @@ public partial class PlayerMove : CharacterBody3D
 	Vector3 to = Vector3.Zero;
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { }
+	public override void _Ready() { }vc
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
